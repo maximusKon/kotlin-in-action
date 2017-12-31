36e128f96def88fb18ddb5960eb5c93a85357df8
maxim_000 <maximovkl@gmail.com>
2017-12-31 17:04:13 +0300

    if в функции eval заменен на when

--- src/main/kotlin/ru/maximov/kotlininaction/ch02/enums/Expressions.kt
+++ src/main/kotlin/ru/maximov/kotlininaction/ch02/enums/Expressions.kt
@@ -9,11 +9,8 @@ fun main(args: Array<String>){
 }
 
 private fun eval(e: Expr): Int =
-    if(e is Num){
-        e.value
-    }
-    else if(e is Sum){
-        eval(e.right) + eval(e.left)
-    } else {
-        throw IllegalArgumentException("Unknown expression")
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
     }
