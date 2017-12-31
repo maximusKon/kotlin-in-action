a4e37328ad873210958bc27b4620beeeb0b342dc
maxim_000 <maximovkl@gmail.com>
2017-12-31 17:14:15 +0300

    в функции eval добавлено логирование

--- src/main/kotlin/ru/maximov/kotlininaction/ch02/enums/Expressions.kt
+++ src/main/kotlin/ru/maximov/kotlininaction/ch02/enums/Expressions.kt
@@ -10,7 +10,15 @@ fun main(args: Array<String>){
 
 private fun eval(e: Expr): Int =
     when (e) {
-        is Num -> e.value
-        is Sum -> eval(e.right) + eval(e.left)
+        is Num -> {
+            println("num: ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = eval(e.left)
+            val right = eval(e.right)
+            println("sum: $left + $right")
+            left + right
+        }
         else -> throw IllegalArgumentException("Unknown expression")
     }
