0aeb07e2d22ff4cf24a2a561e5f4dc04109992b1
maxim_000 <maximovkl@gmail.com>
2017-12-31 17:01:11 +0300

    блочное тело функции eval заменено на тело-выражение

--- src/main/kotlin/ru/maximov/kotlininaction/ch02/enums/Expressions.kt
+++ src/main/kotlin/ru/maximov/kotlininaction/ch02/enums/Expressions.kt
@@ -8,13 +8,12 @@ fun main(args: Array<String>){
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
 }
 
-private fun eval(e: Expr): Int{
+private fun eval(e: Expr): Int =
     if(e is Num){
-        val n = e as Num
-        return n.value
+        e.value
     }
-    if(e is Sum){
-        return eval(e.right) + eval(e.left)
+    else if(e is Sum){
+        eval(e.right) + eval(e.left)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
     }
-    throw IllegalArgumentException("Unknown expression")
-}
