f84ce80112b7fd569495402ac8a06f78cb8f3011
maxim_000 <maximovkl@gmail.com>
2018-01-30 19:58:50 +0300

    Параметры по умолчанию

--- src/main/kotlin/ru/maximov/kotlininaction/ch03/JointToString.kt
+++ src/main/kotlin/ru/maximov/kotlininaction/ch03/JointToString.kt
@@ -2,14 +2,15 @@ package ru.maximov.kotlininaction.ch03
 
 fun main(args: Array<String>) {
     println(joinToString(listOf(1, 2, 3), ";", "(", ")"))
-    println(joinToString(listOf(1, 2, 3), sepearator = " ", prefix = "", postfix = ""))
+    println(joinToString(listOf(1, 2, 3), sepearator = " ", prefix = "[", postfix = "]"))
+    println(joinToString(listOf(1, 2, 3), sepearator = " ", postfix = "]"))
 }
 
 fun <T> joinToString(
         collection: Collection<T>,
-        sepearator: String,
-        prefix: String,
-        postfix: String
+        sepearator: String = ",",
+        prefix: String = "",
+        postfix: String = ""
 ): String {
     val result = StringBuilder(prefix)
 
