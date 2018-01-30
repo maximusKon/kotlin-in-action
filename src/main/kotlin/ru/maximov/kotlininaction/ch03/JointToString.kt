59229c81082e854b980ccc333c847e8fa4b17693
maxim_000 <maximovkl@gmail.com>
2018-01-30 19:47:42 +0300

    Именованые аргументы

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch03/JointToString.kt
@@ -0,0 +1,24 @@
+package ru.maximov.kotlininaction.ch03
+
+fun main(args: Array<String>) {
+    println(joinToString(listOf(1, 2, 3), ";", "(", ")"))
+    println(joinToString(listOf(1, 2, 3), sepearator = " ", prefix = "", postfix = ""))
+}
+
+fun <T> joinToString(
+        collection: Collection<T>,
+        sepearator: String,
+        prefix: String,
+        postfix: String
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(sepearator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+
+    return result.toString()
+}
