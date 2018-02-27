1a10d890125eeb1c0052ac0bb7136fdfee57b52c
maxim_000 <maximovkl@gmail.com>
2018-02-27 21:57:33 +0300

    изменение видимости методов доступа

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch04/AccessMethods.kt
@@ -0,0 +1,18 @@
+package ru.maximov.kotlininaction.ch04
+
+class LengthCounter {
+
+    var counter: Int = 0
+        private set
+
+    fun addWord(word: String) {
+        counter += word.length
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val lengthCounter = LengthCounter()
+    lengthCounter.addWord("Hi!")
+    println(lengthCounter.counter)
+}
