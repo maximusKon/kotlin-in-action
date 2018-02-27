e606df0441a230144f991a182d4ba148afcd8bcf
maxim_000 <maximovkl@gmail.com>
2018-02-27 22:32:26 +0300

    универсальные методы объектов

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch04/UniversalObjectMethods.kt
@@ -0,0 +1,13 @@
+package ru.maximov.kotlininaction.ch04.universalobjectmethods
+
+class Client(val name: String, val postalCode: Int) {
+
+    override fun equals(other: Any?) =
+            other is Client &&
+                    name == other.name && postalCode == other.postalCode
+
+    override fun hashCode() = name.hashCode() * 31 + postalCode
+
+    override fun toString() = "Client(name=$name, postalCode=$postalCode)"
+
+}
