130b869e4f659088a73379a2b7fd7f59b91a4611
maxim_000 <maximovkl@gmail.com>
2018-03-01 21:01:02 +0300

    модификатор data

--- src/main/kotlin/ru/maximov/kotlininaction/ch04/UniversalObjectMethods.kt
+++ src/main/kotlin/ru/maximov/kotlininaction/ch04/UniversalObjectMethods.kt
@@ -1,13 +1,15 @@
 package ru.maximov.kotlininaction.ch04.universalobjectmethods
 
-class Client(val name: String, val postalCode: Int) {
+//class Client(val name: String, val postalCode: Int) {
+//
+//    override fun equals(other: Any?) =
+//            other is Client &&
+//                    name == other.name && postalCode == other.postalCode
+//
+//    override fun hashCode() = name.hashCode() * 31 + postalCode
+//
+//    override fun toString() = "Client(name=$name, postalCode=$postalCode)"
+//
+//}
 
-    override fun equals(other: Any?) =
-            other is Client &&
-                    name == other.name && postalCode == other.postalCode
-
-    override fun hashCode() = name.hashCode() * 31 + postalCode
-
-    override fun toString() = "Client(name=$name, postalCode=$postalCode)"
-
-}
+data class Client(val name: String, val postalCode: Int)
