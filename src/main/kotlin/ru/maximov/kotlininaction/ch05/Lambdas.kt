36009db2cb8606c70483f85f53102657e8fb5e30
maxim_000 <maximovkl@gmail.com>
2018-03-03 21:02:03 +0300

    синтаксис лямбда-выражений

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch05/Lambdas.kt
@@ -0,0 +1,32 @@
+package ru.maximov.kotlininaction.ch05.lambdas
+
+fun main(args: Array<String>) {
+    val sum = { x: Int, y: Int ->
+        println("Computing the sum of $x and $y")
+        x + y
+    }
+    println(sum(1, 2));
+
+    { println(42) }()
+
+    run { println(42) }
+
+
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    println(people.maxBy({ p: Person -> p.age }))
+    println(people.maxBy { p: Person -> p.age })
+    println(people.maxBy { p: Person -> p.age })
+    println(people.maxBy { p -> p.age })
+    println(people.maxBy { it.age })
+    val getAge = { p: Person -> p.age }
+    println(people.maxBy(getAge))
+
+    println(people.joinToString(separator = " ",
+            transform = { p: Person -> p.name })
+    )
+
+    val namesString = people.joinToString(" ") { p: Person -> p.name }
+    println(namesString)
+}
+
+data class Person(val name: String, val age: Int)
