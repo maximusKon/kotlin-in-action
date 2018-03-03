29cd220507771cd2fe7e9a2cd61d290944d8c913
maxim_000 <maximovkl@gmail.com>
2018-03-03 12:26:02 +0300

    объекты-компаньоны

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch04/objects/ObjectCompanion.kt
@@ -0,0 +1,58 @@
+package ru.maximov.kotlininaction.ch04.objects.objectcompanion
+
+fun main(args: Array<String>) {
+    A.bar()
+
+    val subscribingUser = User.newSubscribingUser("bob@gmail.com")
+    val facebookUser = User.Companion.newFacebookUser(4)
+    println(subscribingUser)
+    println(facebookUser)
+
+    println(facebookUser.copy())
+    println(facebookUser === facebookUser.copy())
+
+    loadFromJSON(Person)
+
+    val c = Client.fromJSON("JSON_TEXT_STUB")
+}
+
+class A {
+
+    companion object {
+        fun bar() {
+            println("Companion object called")
+        }
+    }
+
+}
+
+data class User private constructor(val nickname: String) {
+    companion object {
+        fun newSubscribingUser(email: String) =
+                User(email.substringBefore('@'))
+
+        fun newFacebookUser(accountId: Int) =
+                User(getFacebookName(accountId))
+
+        private fun getFacebookName(accountId: Int) = "Facebook Name $accountId"
+    }
+}
+
+interface JSONFactory<T> {
+    fun fromJSON(jsonText: String): T
+}
+
+class Person(val name: String) {
+    companion object : JSONFactory<Person> {
+        override fun fromJSON(jsonText: String): Person = Person("Json Stub")
+
+    }
+}
+
+fun <T> loadFromJSON(factory: JSONFactory<T>): T = factory.fromJSON("{}")
+
+class Client(val firstName: String, val lastName: String) {
+    companion object
+}
+
+fun Client.Companion.fromJSON(jsonText: String) = Client("FN JSON STUB", "LN JSON STUB")
