80979f8a1827e4d902822b5fefb439f4709dcd81
maxim_000 <maximovkl@gmail.com>
2018-03-11 15:43:07 +0300

    безопасное приведение типов

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch06/NullableAs.kt
@@ -0,0 +1,24 @@
+package ru.maximov.kotlininaction.ch06.nullableas
+
+fun main(args: Array<String>) {
+    val p1 = Person("Dmitry", "Jemerov")
+    val p2 = Person("Dmitry", "Jemerov")
+
+    println(p1 == p2)
+
+    println(p1.equals(42))
+}
+
+class Person(val firstName: String, val lastName: String) {
+
+    override fun equals(other: Any?): Boolean {
+        val otherPerson = other as? Person ?: return false
+
+        return otherPerson.firstName == this.firstName &&
+                otherPerson.lastName == this.lastName
+    }
+
+    override fun hashCode(): Int =
+            firstName.hashCode() * 37 + lastName.hashCode()
+}
+
