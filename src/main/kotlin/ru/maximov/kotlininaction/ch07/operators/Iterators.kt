f1c7c95ff9e83118dbdcee732cb7f2dc31ca8f46
maxim_000 <maximovkl@gmail.com>
2018-04-06 18:25:24 +0300

    Соглашение iterator для цикла for

---
+++ src/main/kotlin/ru/maximov/kotlininaction/ch07/operators/Iterators.kt
@@ -0,0 +1,25 @@
+package ru.maximov.kotlininaction.ch07.operators
+
+import java.time.LocalDate
+
+fun main(args: Array<String>) {
+    val newYear = LocalDate.ofYearDay(2017, 1)
+    val daysOff = newYear.minusDays(1)..newYear
+    for (dayOff in daysOff) {
+        println(dayOff)
+    }
+}
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
+        object : Iterator<LocalDate> {
+            var current = start
+
+            override fun hasNext() =
+                    current <= endInclusive
+
+            override fun next() = current.apply {
+                current = plusDays(1)
+            }
+
+
+        }
