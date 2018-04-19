2b7d53d8adf6b477f6d7edfefb787feac9d306bc
maxim_000 <maximovkl@gmail.com>
2018-04-19 23:38:42 +0300

    эксперименты с return

--- src/main/kotlin/ru/maximov/kotlininaction/ch08/AnonimusFunctions.kt
+++ src/main/kotlin/ru/maximov/kotlininaction/ch08/AnonimusFunctions.kt
@@ -6,9 +6,16 @@ fun main(args: Array<String>) {
     people.lookForAlice()
 
     println(people.filter(fun(person) = person.age < 30))
+
+    //println(people.filter{ return it.age < 30})
+    //println(people.notInlinedFiler { return it.age < 30})
 }
 
 data class Person(val name: String, val age: Int)
+
+fun List<Person>.notInlinedFiler(predicate: (Person) -> Boolean) {
+    this.filter(predicate)
+}
 
 fun List<Person>.lookForAlice() {
     this.forEach(fun(person) {
